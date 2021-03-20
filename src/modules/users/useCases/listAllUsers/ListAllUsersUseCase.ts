49cbf8a08faea9538e8556dc6e2f494ea7ecbac3
alisson-moura <alisson.mo.moura@outlook.com.br>
2021-03-20 17:26:09 -0300

    desafio finalizado

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,21 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
     // Complete aqui
+    const user = this.usersRepository.findById(user_id)
+    if (user.admin == false) {
+      throw new Error('Usuário não é admin')
+    }
+    if (!user) {
+      throw new Error('Usuário não está cadastrado')
+    }
+
+
+    const users = this.usersRepository.list()
+    return users
   }
 }
 
